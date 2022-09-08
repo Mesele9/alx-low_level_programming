235de97374b93b6734bd008430fec4f63d63471c
Mesele <getmesy@gmail.com>
2022-09-08 20:19:45 +0000

    push 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints a string using puts fuction'
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzle\n");
+	return (0);
+}
