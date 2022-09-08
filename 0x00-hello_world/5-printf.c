3a6e6b8be6e3a3d92b5d7a0d8975af2deaeb8d30
Mesele <getmesy@gmail.com>
2022-09-08 20:40:21 +0000

    push 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints exactly with proper
+ * grammer, but the outcome is piece of an art
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
