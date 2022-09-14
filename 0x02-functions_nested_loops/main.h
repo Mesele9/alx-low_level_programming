01b2bbd4f8edb742319edc53e930d3dfed67f064
Mesele <getmesy@gmail.com>
2022-09-14 17:43:19 +0000

    add advanced files and make correction

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
+
 /**
- * File: main.h
- *
  * Description: header file containing functions declaration
+ *@c: character to print
  */
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -16,3 +16,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int);
+void print_times_table(int n);
