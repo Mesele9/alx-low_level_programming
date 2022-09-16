51211a892f05f7ed058443aaeaa11406e1d0fbac
Mesele <getmesy@gmail.com>
2022-09-16 02:18:16 +0000

    push 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - fucntion that multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
