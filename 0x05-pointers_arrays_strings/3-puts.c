b405217285b3a537ece00616bd20752699300d96
Mesele <getmesy@gmail.com>
2022-09-20 17:53:22 +0000

    betty 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * _puts -  a function that prints a string
- * @*str: a ponter to a string
+ * @str: string to print
+ *
  */
 void _puts(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
