f08d8f00a134092b45481b982747502f2dd171b8
Mesele <getmesy@gmail.com>
2022-09-20 18:10:34 +0000

    push 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string in reverse
+ * @s: string to be printed in reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int i,j;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar(10);
+}
