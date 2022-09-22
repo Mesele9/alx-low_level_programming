41ff45eeaeac2d0f912dd5768a093e4b1c6cdcb6
Mesele <getmesy@gmail.com>
2022-09-22 01:28:09 +0000

    oush 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase letters to uppercase
+ * @s: string to change case
+ *
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
