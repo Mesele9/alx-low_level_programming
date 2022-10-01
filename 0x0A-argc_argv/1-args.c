4c3b634691d2bdab2fb59dd390086d6745060a28
Mesele <getmesy@gmail.com>
2022-10-01 14:21:22 +0000

    push 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - a program that prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
