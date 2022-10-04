be7ed8dcbf54b769d603366f739e0784e4aedd1e
Mesele <getmesy@gmail.com>
2022-10-04 15:52:09 +0000

    push 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		while (i < len1)
 		{
-			s3[i] = s1[i];													i++													}
+			s3[i] = s1[i];													i++;													}
 															}
 
 	if (s2)
