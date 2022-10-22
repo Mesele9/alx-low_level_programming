056d5d11a7985b94f2539182559e95fe6de89c72
Mesele <getmesy@gmail.com>
2022-10-22 02:33:29 +0000

    push 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - function that return the number of
+ * elements in a linked list list_t list
+ * @h: a pointer to list_t list
+ *
+ * Return: number of elemet in a list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len)
+
+}
