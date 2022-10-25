0de6e64100992c6f254266cab6e48a3c24e9a23a
Mesele <getmesy@gmail.com>
2022-10-25 03:49:44 +0000

    push 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_number - a function that prints an integer
+ * @n: number to be printed
+ */
+void print_number(int n)
+{
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n == 0)
+		_putchar ('0');
+
+	if (n/10)
+		print_number(n/10);
+	_putchar(n%10 + '0');
+	
+}
+
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(-1024);
+	_putchar('\n');
+	print_number(-6);
+	_putchar('\n');
+	return (0);
+}
