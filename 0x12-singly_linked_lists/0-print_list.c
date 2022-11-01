ac091e29ebd08c5b03baf865ab3bb53fe76bad85
Mesele <getmesy@gmail.com>
2022-11-01 17:33:57 +0000

    correctiob

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 /**
  * print_list - afunction that prints all the elements of a list_t list
  * @h: pointer to the list_tlist to print
