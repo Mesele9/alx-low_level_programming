a94e45e97162f09797d053826668c2ffc6d402e2
Mesele <getmesy@gmail.com>
2022-11-02 17:10:10 +0000

    push 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,15 +12,13 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *temp;
 
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
-		temp = head;
 	}
 
-	if (temp == NULL)
+	if (head == NULL)
 		return (NULL);
-	return (temp);
+	return (head);
 }
