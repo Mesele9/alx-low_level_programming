37b406c77bb6e8c73c75be5bd427dfb20ed89385
Mesele <getmesy@gmail.com>
2022-11-02 01:51:35 +0000

    push 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * listint_len - a function that returns the number of elements
+ * ina linked list list_t
+ * @h: linked list to be checked
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i;
+
+	if (!h)
+		return (0);
+
+	i= 0;
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
