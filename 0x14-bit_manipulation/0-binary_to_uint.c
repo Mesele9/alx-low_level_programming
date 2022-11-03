295121f6190fb5076c57c94335bc7bf8c08ee5a1
Mesele <getmesy@gmail.com>
2022-11-03 17:35:45 +0000

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,22 +13,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int val = 0;
-	int i = 0;
+	unsigned int total, power;
+	int len;
 
 	if (b == NULL)
 		return (0);
 
-
-	while (b[i] == '0' || b[i] == '1')
+	for (len = 0; b[len]; len++)
 	{
-		if (b[i] != '0' || b[i] != '1')
-		{
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		}
-		val <<= 1;
-		val += b[i] - '0';
-		i++;
 	}
-	return (val);
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+	return (total);
 }
